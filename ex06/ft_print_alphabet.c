29556ad27511cd2feabf078f1ad2a39cc48930e7
alaamimi <aku-no-hana@outlook.fr>
2021-02-09 14:05:11 +0100

    More probs

---
+++ ex06/ft_print_alphabet.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/27 01:19:48 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/01/27 01:24:58 by alaamimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+void	ft_putchar(char c);
+
+void ft_print_alphabet(void)
+{
+	char letter;
+
+	letter = 'a';
+	while(letter <= 'z')
+	{
+		ft_putchar(letter);
+		letter++;
+	}
+}
