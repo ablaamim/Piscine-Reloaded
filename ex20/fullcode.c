96b901208149f62e334969f8b842704cb71e8b7f
alaamimi <aku-no-hana@outlook.fr>
2021-02-18 00:16:57 +0100

    ft_strdup

---
+++ ex20/fullcode.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alaamimi <alaamimi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/17 23:16:03 by alaamimi          #+#    #+#             */
+/*   Updated: 2021/02/18 00:15:41 by alaamimi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int ft_strlen(char *str)
+{
+	int len;
+
+	len = 0;
+	while(str[len] != '\0')
+	{
+		len++;
+	}
+	return(len);
+}
+
+char	*ft_strdup(char *s1)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	dest = malloc(sizeof(*s1) * ((ft_strlen((char *)s1)) + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+int main(void)
+{
+	char str[] = "Duplicate me motherfucker";
+
+	ft_putstr(str);
+		ft_putchar('\n');
+	printf("%p\n", str);
+	write(1, "-----\n", 6);
+	ft_strdup(str);
+	ft_putstr(str);
+		ft_putchar('\n');
+	printf("%p\n", str);
+	write(1, "-----\n", 6);
+	return EXIT_SUCCESS;
+}
